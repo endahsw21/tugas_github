6e2ab0ffc93dfbc9999bf87dcfaccacb8bbfc951
endahsw21 <132032980+endahsw21@users.noreply.github.com>
2023-04-28 11:10:51 +0700

    Membuat Biodata.php
    Deskripsi Nama, Tempat Tanggal Lahir dan Alamat

---
+++ biodata.php
@@ -0,0 +1,12 @@
+<html>
+<head>      
+    <title>Biodata</title>     
+</head>  
+  
+<body>        
+<h1>Biodata diri</h1>               
+<p>Nama : Endah Sarwosri</p>           
+<p>Tempat/Tanggal Lahir : Wonogiri, 21 Juli 2000</p>        
+<p>Alamat : Bulusulur, Wonogiri </p>  
+</body>
+</html>
