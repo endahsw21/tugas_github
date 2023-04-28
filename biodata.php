67c507b8ca13ff0ae32db1894b4b056ff452d904
endahsw21 <132032980+endahsw21@users.noreply.github.com>
2023-04-28 11:16:55 +0700

    Update script line 10-12 biodata.php
    Tambah Deskripsi Alamat, Kewarganegaraan dan Email

--- biodata.php
+++ biodata.php
@@ -7,6 +7,8 @@
 <h1>Biodata diri</h1>               
 <p>Nama : Endah Sarwosri</p>           
 <p>Tempat/Tanggal Lahir : Wonogiri, 21 Juli 2000</p>        
-<p>Alamat : Bulusulur, Wonogiri </p>  
+<p>Alamat : Klemut RT 01/RW 07, Bulusulur, Wonogiri </p>
+<p>Kewarganegaraan : Indonesia</p>
+<p>Email : endah.sarwosri@yahoo.com </p>
 </body>
 </html>
