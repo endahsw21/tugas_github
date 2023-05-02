739350261db0a335bd81d3d15cf4e7dcefcf11bc
Endah Sarwosri <132032980+endahsw21@users.noreply.github.com>
2023-05-02 10:18:55 +0700

    Update Biodata2

--- biodata.php
+++ biodata.php
@@ -1,10 +1,14 @@
 <html>
 <head>      
-    <title>Biodata</title>     
+    <title>Biodata</title>
+<style>
+h1   {color: blue;}
+p    {color: red;}
+</style>
 </head>  
   
 <body>        
-<h1>Biodata diri</h1>               
+<h1>Biodata Diri</h1><hr>               
 <p>Nama : Endah Sarwosri</p>           
 <p>Tempat/Tanggal Lahir : Wonogiri, 21 Juli 2000</p>        
 <p>Alamat : Klemut RT 01/RW 07, Bulusulur, Wonogiri </p>
